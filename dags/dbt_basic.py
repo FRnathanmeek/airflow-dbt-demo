82c35b2bac1a9f0130b42200d5b06d5eeb2b8487
FRnathanmeek <nathan.meek@firstresonance.io>
2023-08-23 14:34:52 -0700

    update

--- dags/dbt_basic.py
+++ dags/dbt_basic.py
@@ -8,10 +8,19 @@ from pendulum import datetime
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+import logging
 
 # We're hardcoding this value here for the purpose of the demo, but in a production environment this
 # would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "/usr/local/airflow/dbt"
+DBT_PROJECT_DIR = "/usr/local/airflow/dbt/"
+
+
+def list_available_files():
+    import os
+    for path, subdirs, files in os.walk('.'):
+        for name in files:
+            print(os.path.join(path, name))
 
 
 with DAG(
@@ -21,30 +30,41 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     default_args={
-        "env": {
-            "DBT_USER": "{{ conn.postgres.login }}",
-            "DBT_ENV_SECRET_PASSWORD": "{{ conn.postgres.password }}",
-            "DBT_HOST": "{{ conn.postgres.host }}",
-            "DBT_SCHEMA": "{{ conn.postgres.schema }}",
-            "DBT_PORT": "{{ conn.postgres.port }}",
-        }
+        # "env": {
+        #     "DBT_USER": "{{ conn.postgres.login }}",
+        #     "DBT_ENV_SECRET_PASSWORD": "{{ conn.postgres.password }}",
+        #     "DBT_HOST": "{{ conn.postgres.host }}",
+        #     "DBT_SCHEMA": "{{ conn.postgres.schema }}",
+        #     "DBT_PORT": "{{ conn.postgres.port }}",
+        # }
     },
 ) as dag:
     # This task loads the CSV files from dbt/data into the local postgres database for the purpose of this demo.
     # In practice, we'd usually expect the data to have already been loaded to the database.
-    dbt_seed = BashOperator(
-        task_id="dbt_seed",
-        bash_command=f"dbt seed --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+    # dbt_seed = BashOperator(
+    #     task_id="dbt_seed",
+    #     bash_command=f"dbt seed --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+    # )
+
+    list_files = PythonOperator(
+        task_id="list_files",
+        python_callable=list_available_files,
     )
 
     dbt_run = BashOperator(
         task_id="dbt_run",
-        bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+        bash_command="cd ${AIRFLOW_HOME}/dbt && dbt run --profiles-dir . --select issues_staged",
+        # bash_command="cd ${AIRFLOW_HOME}/dbt && ls -a",
     )
 
-    dbt_test = BashOperator(
-        task_id="dbt_test",
-        bash_command=f"dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+    # dbt_test = BashOperator(
+    #     task_id="dbt_test",
+    #     bash_command=f"dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+    # )
+
+    ready = BashOperator(
+        task_id="ready",
+        bash_command="echo 'Ready!'",
     )
 
-    dbt_seed >> dbt_run >> dbt_test
+    list_files >> dbt_run >> ready
